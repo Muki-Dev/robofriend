01e716794fc5e6933dfd5cd423571c82a3ee5b39
Muki Dev <mukidev881@gmail.com>
2024-08-01 23:43:42 +0000

    redux Async Actions

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware,combineReducers } from 'redux';
 import { createLogger } from 'redux-logger';
+import  { thunk } from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
-import { searchRobots } from './reducers';
+import { searchRobots,requestRobots } from './reducers';
 import 'tachyons';
 
 const logger = createLogger();
-const store = createStore(searchRobots, applyMiddleware(logger));
+const rootReducer = combineReducers({ searchRobots,requestRobots })
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
